927766539d95bb6b263c374d51c67bb16a6df41b
toki <toki.kanno@gmail.com>
2014-09-01 16:15:06 +0800

    edit starup.sh for external volume usage

--- mysql/startup.sh
+++ mysql/startup.sh
@@ -1,18 +1,15 @@
 #/bin/bash
 
-# origin from: https://gist.github.com/benschw/7391723
- 
 if [ ! -f /var/lib/mysql/ibdata1 ]; then
- 
-#    mysql_install_db
- 
+    mysql_install_db
+
     /usr/bin/mysqld_safe &
     sleep 10s
- 
-    echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '54323013' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
- 
+
+    echo "GRANT ALL ON *.* TO 'admin'@'%' IDENTIFIED BY '54323013' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
     killall mysqld
     sleep 10s
 fi
- 
+
 /usr/bin/mysqld_safe
